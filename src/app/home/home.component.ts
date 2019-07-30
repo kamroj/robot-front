55ca11cafc854d371ae959a0a4b5dcc6114434ba
Kapaika <bartosz.kupajski@gmail.com>
2019-07-30 12:01:52 +0200

    Sprint#3, Task#200, dodanie subscribe do startu robota

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit {
 
 
   start() {
-    this.httpClientService.startRobot();
+    this.httpClientService.startRobot().subscribe();
   }
 
   startRobot() {
