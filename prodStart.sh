3aae39973d0a693bd48ac3ea6c5ad572c7f2f256
Kapaika <bartosz.kupajski@gmail.com>
2019-08-28 14:21:58 +0200

    Sprint#6 Task#9 zmienia port w skrypcie na 4200

--- prodStart.sh
+++ prodStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A script which will run this application on http://localhost:8081
+# A script which will run this application on http://localhost:4200
 ng build --aot --prod
 xdg-open http://localhost:4200
 node server.js
